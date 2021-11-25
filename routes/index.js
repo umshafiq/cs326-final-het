4504e4d0ecea56a59fb09e32bc5339ef548e8226
Umar Shafiq <umshafiq@gmail.com>
2021-11-25 04:29:24 -0500

    Group assignment code.

--- routes/index.js
+++ routes/index.js
@@ -46,6 +46,44 @@ router.get('/group/:id/items', auth.isLoggedIn, function(req, res, next) {
         });
 });
 
+router.get('/group/:id/assign', auth.isLoggedIn, function(req, res, next) {
+  db.any(`SELECT * FROM users INNER JOIN group_users ON group_users.user_id = users.id WHERE group_users.group_id = $1`, [req.params.id])
+      .then((users) => {
+          let user_is_admin = false;
+          let group_unassigned = true;
+          for (user of users) {
+              if (user.id === req.user.id && user.is_admin) {
+                  user_is_admin = user.is_admin;
+              }
+              if (user.giftee_id !== null) {
+                  group_unassigned = false;
+              }
+          }
+          if (!user_is_admin) {
+              res.status(400).render('error');
+              return;
+          }
+          if (!group_unassigned) {
+              res.status(400).render('error');
+              return;
+          }
+          // if we made it this far, it's time to do the shuffle
+          // amazingly, this can be done in one line of SQL
+          db.none(`UPDATE group_users SET giftee_id = subtable1.coalesce FROM (SELECT user_id, COALESCE(lead(user_id) OVER (ORDER BY r), first_value(user_id) OVER (ORDER BY r)) FROM (SELECT random() as r, user_id FROM group_users WHERE group_id = $1) AS subtable) AS subtable1 WHERE group_users.group_id = $1 and group_users.user_id = subtable1.user_id`, [req.params.id])
+              .then(() => {
+                  res.redirect('/group/' + req.params.id.toString());
+              })
+              .catch(err => {
+                  res.render('error');
+              });
+      })
+      .catch(err => {
+          res.render('error');
+      });
+});
+
+
+
 router.get('/giftee', auth.isLoggedIn, function(req, res, next) {
   res.render('giftee', { title: 'Giftee' });
 });
