bd768a540fa501ccd7dadb8f2b1e327b51259979
Umar Shafiq <umshafiq@gmail.com>
2021-11-25 01:56:10 -0500

    Actually added the auth middleware this time.

---
+++ auth.js
@@ -0,0 +1,14 @@
+const db = require('database');
+
+let auth = (req, res, next) => {
+    let user_id = req.cookies.id;
+    db.one(`SELECT * FROM users WHERE id = $1`, [user_id])
+        .then(() => {
+            next();
+        });
+        .catch(err => {
+            next(err);
+        });
+};
+
+module.exports = { auth };
