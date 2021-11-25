dc59ef00000925312913216383953b016df7612f
Umar Shafiq <umshafiq@gmail.com>
2021-11-25 02:18:40 -0500

    import -> require is complicated

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const auth = require('../auth');
+const auth = require('../auth').auth;
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Login' });
